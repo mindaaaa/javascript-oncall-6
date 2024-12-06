a87fdee0c900c36383f756f18731f982a3d1e52d
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-06 19:08:15 +0900

    feat(Day): 요일 정보를 가져오고 휴일 유무를 판단할 수 있도록 로직 구현

--- src/domain/Day.js
+++ src/domain/Day.js
@@ -1,27 +1,59 @@
-// [1,1] [월,일]형식 배열
+// Date 객체
+// 월이 갖고 있는 모든 일은 Day 클래스다.
+// 평일인지 휴일인지를 판단한다.
+// 배열 형태로 바꿔준다.
+
+// 0은 1월
+// 0은 일 1은 월
 class Day {
-  #month;
-  #day;
-  #holiday;
-  constructor(dayArray) {
-    this.#month = dayArray[0];
-    this.#day = dayArray[1];
-    this.#holiday = this.#isHoliday();
+  #date;
+  #weekday; // 요일 정보
+  #dayOff; // 휴일인지
+
+  constructor(date) {
+    this.#date = date; // Date 객체
+    this.#weekday = this.getWeekday(); // 요일 정보 ['일']
+    this.#dayOff = this.#isDayOff(); // 휴일인지
   }
+
   #holidays = [
-    { month: 1, day: 1, name: 신정 },
-    { month: 3, day: 1, name: 삼일절 },
-    { month: 5, day: 5, name: 어린이날 },
-    { month: 6, day: 6, name: 현충일 },
-    { month: 8, day: 15, name: 광복절 },
-    { month: 10, day: 3, name: 개천절 },
-    { month: 10, day: 9, name: 한글날 },
-    { month: 12, day: 25, name: 성탄절 },
+    { month: 1, day: 1, name: '신정' },
+    { month: 3, day: 1, name: '삼일절' },
+    { month: 5, day: 5, name: '어린이날' },
+    { month: 6, day: 6, name: '현충일' },
+    { month: 8, day: 15, name: '광복절' },
+    { month: 10, day: 3, name: '개천절' },
+    { month: 10, day: 9, name: '한글날' },
+    { month: 12, day: 25, name: '성탄절' },
   ];
 
-  #isHoliday() {
+  isHoliday() {
     return this.#holidays.some((holiday) => {
-      return holiday.month === this.#month && holiday.day === this.#day;
+      return (
+        holiday.month === this.getMonth() &&
+        holiday.day === this.#date.getDate()
+      );
     });
   }
+
+  getMonth() {
+    return this.#date.getMonth() + 1;
+  }
+
+  #isDayOff() {
+    return this.isHoliday() || this.#isWeekend();
+  }
+
+  #isWeekend() {
+    // 0이거나 6인지 확인
+    return this.#date.getDay() === 0 || this.#date.getDay() === 6;
+  }
+
+  getWeekday() {
+    const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+    return weekdays[this.#date.getDay()];
+  } // 요일 정보를 가져옴
 }
+
+// 월 정보 가져와야함
+// 요일 정보 가져와야함
