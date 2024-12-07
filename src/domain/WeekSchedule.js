75f83548d7a20fcb00544de1c7064c58dbd1dbf2
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-07 18:05:34 +0900

    refactor(WeekSchedule): 근무자 배정 로직 리팩토링

--- src/domain/WeekSchedule.js
+++ src/domain/WeekSchedule.js
@@ -10,35 +10,53 @@ class WeekSchedule {
   }
 
   assignWorkers(days) {
-    // day를 이용해 근무를 배정한다.
-    // dayOff로 휴일/평일을 나눔
-    // 평일과 휴일 순번에 따라 근무자 배정
-    // weekdaySchedule, holidaySchedule
-    // 연속 근무자 방지
     let previousWorker = null; // 전날 근무자
 
     days.forEach((day) => {
       let assignedWorker;
+      let holidayIndex; // holiday 배열 index 저장
+      let weekdayIndex; // weekDay 배열 index 저장
 
       if (day.dayOff) {
-        // 휴일 근무자 배정
+        this.#assignHolidayWorker(); // 여기서 holidayIndex 넘겨줘야하나?
       }
-      // 평일 근무자 배정
 
+      this.#assignWeekdayWorker();
+
+      // swap하기
+      // this.#holidaySchedule.splice(holidayIndex, 1, this.#holidayScehedule[holid])
+      // 연속 근로자 방지 로직
       if (assignedWorker === previousWorker) {
         if (day.dayOff) {
           // 다음 휴일 근무자 배정
+          this.#holidaySchedule.splice(
+            holidayIndex,
+            1,
+            this.#holidaySchedule[
+              (holidayIndex + 1) % this.#holidaySchedule.length
+            ] // 수아 루루 글로 -> 루루 수아 글로
+          );
+          assignedWorker = this.#holidaySchedule[currentIndex];
         }
         // 다음 평일 근무자 배정
+        const currentIndex = this.#weekdaySchedule.indexOf(assignedWorker);
+        this.#weekdaySchedule.splice(
+          currentIndex,
+          1,
+          this.#weekdaySchedule[
+            (currentIndex + 1) % this.#weekdaySchedule.length
+          ]
+        );
+        assignedWorker = this.#weekdaySchedule[currentIndex];
       }
-      previousWorker = assignedWorker;
-
       const scheduleEntry = {
         month: day.dateArray[0],
         day: day.dateArray[1],
         weekday: day.weekday,
         worker: assignedWorker,
       };
+
+      previousWorker = assignedWorker;
 
       if (day.isHoliday()) {
         scheduleEntry.note = '(휴일)';
@@ -47,6 +65,32 @@ class WeekSchedule {
       this.#schedule.push(scheduleEntry);
     });
     return this.#schedule;
+  }
+
+  #assignHolidayWorker() {
+    if (!previousWorker) {
+      assignedWorker = this.#holidaySchedule[0];
+    }
+    assignedWorker = this.#holidaySchedule.indexOf(
+      this.#holidaySchedule[holidayIndex + 1]
+    );
+  }
+
+  #assignWeekdayWorker(day) {
+    if (!previousWorker) {
+      assignedWorker = this.#weekdaySchedule[0];
+    }
+    assignedWorker = this.#weekdaySchedule.indexOf(
+      this.#holidaySchedule[weekdayIndex + 1]
+    );
+  }
+
+  #resolveHoliday() {
+    // TODO: 휴일 중복 방지
+  }
+
+  #resolveWeekday() {
+    // TODO: 평일 중복 방지
   }
 }
 
