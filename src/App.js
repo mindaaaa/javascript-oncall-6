7af2d0e0a2980eca6e3f0a4fcd6fff2d0948f069
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-08 12:07:51 +0900

    feat(App): 시작 월과 요일 검증하고 값을 받는 메서드 추가

--- src/App.js
+++ src/App.js
@@ -12,8 +12,22 @@ class App {
     } catch (error) {}
   }
 
-  //   비상 근무를 배정할 월과 시작 요일을 입력하세요> 1,금
-  async getMonthAndDay() {
+  // [Number(month), startDay];
+  async getValidatedMonthAndDay() {
+    while (true) {
+      try {
+        const monthAndDay = await this.#getMonthAndDay();
+        const validator = new Validator(monthAndDay);
+        validator.validateMonthAndDay();
+
+        return monthAndDay;
+      } catch (error) {
+        ConsoleOutput.write(error.message);
+      }
+    }
+  }
+
+  async #getMonthAndDay() {
     const monthAndStartDay = await ConsoleInput.read(
       '비상 근무를 배정할 월과 시작 요일을 입력하세요\n'
     );
@@ -24,10 +38,10 @@ class App {
   }
 
   // TODO: 평일 순번 또는 휴일 순번의 입력 값이 올바르지 않은 경우, '평일 순번'부터 다시 입력 받는다.
-  async getShiftOrder() {}
+  async #getShiftOrder() {}
 
   // 평일 비상 근무 순번대로 사원 닉네임을 입력하세요> 준팍,도밥,고니,수아,루루,글로
-  async getWeekdayShiftOrder() {
+  async #getWeekdayShiftOrder() {
     const weekdayShift = await ConsoleInput.read(
       '평일 비상 근무 순번대로 사원 닉네임을 입력하세요\n'
     );
@@ -35,7 +49,7 @@ class App {
   }
 
   // 휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> 수아,수아,글로,고니,도밥,준팍
-  async getHolidayShiftOrder() {
+  async #getHolidayShiftOrder() {
     const holidayShift = await ConsoleInput.read(
       '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요\n'
     );
